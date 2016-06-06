c72ea1564c0a0eb934164091908020bf528e39b6
luisanegro <lulu.n92@hotmail.com>
2016-06-06 15:00:59 +0000

    Décima-segunda atualização - Luisa: Inclusão do código para a tradução de acordo com a configuração do browser no formulário de cadastro de loja

--- src/CadastroLoja.hs
+++ src/CadastroLoja.hs
@@ -16,28 +16,27 @@ import Database.Persist.Postgresql
 
 mkYesodDispatch "Pagina" pRoutes
 
-
 formLoja :: Form Loja
 formLoja = renderDivs $ Loja <$>
-           areq textField "Nome Fantasia: " Nothing <*>
+           areq textField (fieldSettingsLabel MsgNomeFantasia) Nothing <*>
            areq textField  FieldSettings{fsId=Just "hident2",
                            fsLabel="CNPJ",
                            fsTooltip= Nothing,
                            fsName= Nothing,
                            fsAttrs=[("maxlength","18")]} Nothing <*>
-           areq textField "Logradouro: " Nothing <*>
-           areq textField "Número: " Nothing <*>
+           areq textField  (fieldSettingsLabel MsgLogradouro) Nothing <*>
+           areq textField  (fieldSettingsLabel MsgNumero) Nothing <*>
            areq textField  FieldSettings{fsId=Just "hident5",
                            fsLabel="CEP",
                            fsTooltip= Nothing,
                            fsName= Nothing,
                            fsAttrs=[("maxlength","9")]} Nothing <*> 
-           areq textField "Bairro: " Nothing <*>
-           areq textField "Cidade: " Nothing <*>
-           areq textField "Estado: " Nothing <*>
-           areq textField "Telefone: " Nothing <*>
-           areq textField "E-mail: " Nothing <*>
-           areq textField "Nome do Responsável: " Nothing <*>
+           areq textField  (fieldSettingsLabel MsgBairro) Nothing <*>
+           areq textField  (fieldSettingsLabel MsgCidade) Nothing <*>
+           areq textField  (fieldSettingsLabel MsgEstado) Nothing <*>
+           areq textField  (fieldSettingsLabel MsgTelefone) Nothing <*>
+           areq textField  (fieldSettingsLabel MsgEmail) Nothing <*>
+           areq textField (fieldSettingsLabel MsgNomeDoResponsavel) Nothing <*>
            areq textField  FieldSettings{fsId=Just "hident11",
                            fsLabel="CPF do Responsável",
                            fsTooltip= Nothing,
@@ -47,7 +46,7 @@ formLoja = renderDivs $ Loja <$>
                            fsLabel="RG do Responsável",
                            fsTooltip= Nothing,
                            fsName= Nothing,
-                           fsAttrs=[("maxlength","12")]} Nothing 
+                           fsAttrs=[("maxlength","12")]} Nothing
 
 
 getLojaR :: Handler Html
@@ -104,3 +103,4 @@ getErroR = defaultLayout [whamlet|
               <p>
                 cadastro deu pau com sucesso
 |]
+
